db3d2e0a028538550b04ac540c7ac170b983e47e
Atul Sharma <atul.sharma892@webkul.in>
2023-06-19 17:18:47 +0530

    new feature added

--- wp-content/plugins/my-restapi/includes/front/class_wk_front_function.php
+++ wp-content/plugins/my-restapi/includes/front/class_wk_front_function.php
@@ -12,11 +12,19 @@ register_rest_route(
     'wkmaa/v1',
 				'/page/',
 				array(
-					'methods'             => WP_REST_Server::READABLE,
+					'methods'             => WP_REST_Server::CREATABLE,
 					'callback'            => array( $this, 'get_custom_field' ),
 					'permission_callback' => function () {
 						return true;
 					},
+					'args' => array(
+						'name' => array(
+							'required' => true,
+						),
+						'title' => array(
+							'required' => true
+						)
+					)
 				)
 
     );
@@ -25,7 +33,7 @@ register_rest_route(
 	'wkmaa/v1',
 				'acf-books',
 				array(
-					'method' => 'GET',
+					'method' => WP_REST_Server::READABLE,
 					'callback' => array($this,'wk_acf_books'),
 					'permission_callback' => function () {
 						return true;
@@ -34,15 +42,56 @@ register_rest_route(
 
 );
 
+register_rest_route(
+	'wkmaa/v1',
+				'wk-users',
+				array(
+					'method' => WP_REST_Server::CREATABLE,
+					'callback' => array($this,'wk_users'),
+					'permission_callback' => function () {
+						return true;
+					},
+				)
+				);
+
+register_rest_route( 
+	'wkmaa/v1',
+			'wk-ulist',
+				array(
+					'method' => WP_REST_Server::CREATABLE,
+					'callback' =>array($this,'user_list'),
+					'permission_callback' => function () {
+						return true;
+					},
+					'schema' => 'prefix_get_comment_schema',
+
+				)
+				);
 
 }
+
+
 
 /**
  * Callback function for custom field
  */
-public function get_custom_field()
+public function get_custom_field($request)
 {
-    return "hi";
+$parameter = $request->get_params();
+if($parameter != "")
+{
+return new WP_REST_Response(array(
+	'Message' => 'Success.',
+	'param' => $parameter,
+	'status' => 1,
+), 400);
+} else {
+	return new WP_Error(
+		'no_data_found',
+		'No data Foound',
+	);
+}
+
 }
 
 public function wk_acf_books(WP_REST_Request $request) {
@@ -70,4 +119,28 @@ public function wk_acf_books(WP_REST_Request $request) {
 
 
 }
+
+public function wk_users(WP_REST_Request $request) {
+	$user_login = wp_slash( $request['user_login'] );
+	$user_email = wp_slash( $request['user_email'] );
+	$user_pass  = $request['user_pass'];
+	$userdata = compact( 'user_login', 'user_email', 'user_pass' );
+	$parameter =  wp_insert_user( $userdata );
+	return $parameter;
+
+	
+}
+
+public function user_list(WP_REST_Request $request) {
+	
+global $wpdb;
+$params = get_userdata($request['id']);
+
+return $params;
+
+}
+
+
+
+
 }
